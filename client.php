8f447deac9af08a332c34ae706b2dfbbdbbd94e4
pratham3998 <41316208+pratham3998@users.noreply.github.com>
2019-01-22 22:38:53 +0530

    Added static page of our clients

---
+++ client.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+<title>Clients</title>
+<style>
+a:link, a:visited {
+    background-color: #000000;
+    color: white;
+    padding: 14px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+}
+
+
+a:hover, a:active {
+    background-color: black ;
+}
+</style>
+</head>
+<body>
+</head>
+<body bgcolor="white">
+ 
+<center>
+<img src="http://www.pmexperts.co/uploads/gallery/our-clients_830535_gbanner.jpg" width=100%, height="400">    
+</center>
+<br>
+<br>
+<br>
+<h2>
+<center><h1>
+Our Biggest Clients
+</h1>
+</center>
+</h2>
+<br>
+<br>
+<br>
+<br>
+<br>
+<marquee scrollamount="8">
+<img src="http://www.carlogos.org/logo/Hyundai-logo-silver-2560x1440.png" height="250" width="350">
+<img src="https://i.ytimg.com/vi/lPenrjRWbw8/maxresdefault.jpg" height="250" width="350">
+<img src="http://www.professionalpackersindia.com/img/client3.jpg" height="250" width="350">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuPeV2HHLgzpVKa93toqS-ClR9r8sJySOxM7T137LvRMWQnwK4" height="250" width="350">
+<img src="http://www.logoinstant.com/wp-content/uploads/2015/03/Home-green-energy-logo-640x517.jpg" height="250" width="350">
+<img src="http://www.watchsupermarket.co.uk/media/catalog/product/t/i/timexlogo.jpg" height="250" width="300">
+</marquee>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<marquee scrollamount="8">
+<img src="http://2.bp.blogspot.com/-5ACVHUTK_9E/UoPpo0u13pI/AAAAAAAAcq8/Nz4s4L9eSNQ/s1600/Philips+shield+2013.png" height="250" width="250">
+<img src="https://www.logoinstant.com/wp-content/uploads/2015/02/business-energy-free-logo.jpg" height="250" width="300">
+<img src="https://udayindia.in/wp-content/uploads/2015/07/101-420x252.jpg" height="250" width="300">
+<img src="http://www.bbeinc.com/bbe_site/profile/images/client_logos/covansys.jpg" height="250" width="300">
+<img src="https://cdn.freebiesupply.com/logos/large/2x/nestle-9-logo-png-transparent.png" height="250" width="300">    
+</marquee>
+<br>
+<br>
+<center>
+<a href="website.php"><b>Home</b></a>
+</center>
+</body>
+</html>
